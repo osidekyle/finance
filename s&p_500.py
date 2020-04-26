278e3a5169bdffc424a7b93ff2f002d4dd92beed
Kyle Hawkins <47363388+osidekyle@users.noreply.github.com>
2020-04-26 10:15:16 -0700

    Update s&p_500.py

--- s&p_500.py
+++ s&p_500.py
@@ -5,8 +5,105 @@ import datetime as dt
 import os
 import pandas_datareader.data as web
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+from collections import Counter
+from sklearn import svm, neighbors
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import VotingClassifier,RandomForestClassifier
 
 
+
+style.use('ggplot')
+
+
+def process_data_for_labels(ticker):
+    hm_days=7
+    df=pd.read_csv('sp500_joined_closes.csv',index_col=0)
+    tickers=df.columns.values.tolist()
+    df.fillna(0,inplace=True)
+    for i in range(1,hm_days+1):
+        df['{}_{}d'.format(ticker,i)]=(df[ticker].shift(-i)-df[ticker])/df[ticker]
+    df.fillna(0,inplace=True)
+    return tickers,df
+
+def buy_sell_hold(*args):
+    cols =[c for c in args]
+    requirement=0.02
+    for col in cols:
+        if col > requirement:
+            return 1
+        elif col < -requirement:
+            return -1
+    return 0
+
+def extract_featuresets(ticker):
+    tickers,df=process_data_for_labels(ticker)
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold,
+                                              df['{}_1d'.format(ticker)],
+                                              df['{}_2d'.format(ticker)],
+                                              df['{}_3d'.format(ticker)],
+                                              df['{}_4d'.format(ticker)],
+                                              df['{}_5d'.format(ticker)],
+                                              df['{}_6d'.format(ticker)],
+                                              df['{}_7d'.format(ticker)]
+
+                                              ))
+
+    vals=df['{}_target'.format(ticker)].values.tolist()
+    str_vals=[str(i) for i in vals]
+    #print("Data Spread:",Counter(str_vals))
+    df.fillna(0,inplace=True)
+    df=df.replace([np.inf,-np.inf],np.nan)
+    df.dropna(inplace=True)
+    df_vals=df[[ticker for ticker in tickers]].pct_change()
+    df_vals=df_vals.replace([np.inf,-np.inf],0)
+    df_vals.fillna(0,inplace=True)
+    X=df_vals.values
+    y=df['{}_target'.format(ticker)].values
+    return X,y,df
+
+
+
+def do_ml(ticker):
+    X,y,df=extract_featuresets(ticker)
+    X_train, X_test,y_train,y_test=train_test_split(X,y,test_size=.25)
+    clf=VotingClassifier([('lsvc',svm.LinearSVC()),
+                          ('knn', neighbors.KNeighborsClassifier()),
+                          ('rfor',RandomForestClassifier())])
+    clf.fit(X_train,y_train)
+    confidence=clf.score(X_test,y_test)
+    print('accuracy:', confidence)
+    predictions = clf.predict(X_test)
+    print('predicted class counts:', Counter(predictions))
+    print(y_test)
+    print()
+    print()
+
+
+def visualize_data():
+    df=pd.read_csv("sp500_joined_closes.csv")
+    df_corr=df.corr()
+    print(df_corr.head())
+    df_corr.to_csv("sp500_corr.csv")
+    data1=df_corr.values
+    fig1=plt.figure()
+    ax1=fig1.add_subplot(111)
+    heatmap1=ax1.pcolor(data1,  cmap=plt.cm.RdYlGn)
+    fig1.colorbar(heatmap1)
+    ax1.set_xticks(np.arange(data1.shape[1])+0.5,minor=False)
+    ax1.set_yticks(np.arange(data1.shape[0 ]) + 0.5, minor=False)
+    ax1.invert_yaxis
+    ax1.xaxis.tick_top()
+    column_names=df_corr.columns
+    row_labels=df_corr.index
+    ax1.set_xticklabels(column_names)
+    ax1.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap1.set_clim(-1,1)
+    plt.tight_layout()
+    plt.show()
 def save_sp500_tickers():
     resp=requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup=bs.BeautifulSoup(resp.text,'lxml')
@@ -29,7 +126,7 @@ def get_data_from_yahoo(reload_sp500=False):
             tickers=pickle.load(f)
 
 
-    start=dt.datetime(2019,1,1)
+    start=dt.datetime(2010,1,1)
     end=dt.datetime.now()
 
     for ticker in tickers:
@@ -64,6 +161,8 @@ def compile_data():
     main_df.to_csv("sp500_joined_closes.csv")
 
 
+do_ml("AAPL")
 
 
-compile_data()
+
+
